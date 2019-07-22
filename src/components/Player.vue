34ba6187c5ade82c05b86df97e6a584f143fba5e
Oscar Pérez <mr.omiguleperez@gmail.com>
2019-07-22 06:56:11 -0500

    Add player, comunicate components with plugin

---
+++ src/components/Player.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .content
+    div(v-if="track")
+      p
+        img(:src="track.album.images[0].url ")
+      p
+        strong {{ track.name }}
+        small [{{ track.duration_ms }}]
+      p
+        audio(controls, :src="track.preview_url")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      track: null
+    }
+  },
+
+  created () {
+    this.$bus.$on('set-track', track => {
+      this.track = track
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  img {
+    border-radius: 50%;
+    width: 127px;
+  }
+</style>
+
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -9,4 +9,16 @@
       .container.has-text-centered
         h1.title PlatziMusic
         h2.subtitle Canciones que están Vue-nisímas
+        pm-player
 </template>
+
+<script>
+import PmPlayer from '@/components/Player'
+
+export default {
+  components: {
+    PmPlayer
+  }
+}
+</script>
+
