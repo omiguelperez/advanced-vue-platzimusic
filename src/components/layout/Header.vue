7f9ac7ffd1de910778ae12065a68c79c6be925bc
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-23 06:48:47 -0500

    Add vue-router basic about route

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -5,6 +5,15 @@
         .container
           .navbar-brand
             strong.navbar-item PlatziMusic
+            span.navbar-burger.burger(data-target="navbar-menu")
+              span
+              span
+              span
+          #navbar-menu.navbar-menu
+            .navbar-end
+              router-link.navbar-item(:to="{ name: 'search' }") Buscar
+              router-link.navbar-item(to="about") Nosotros
+        
     .hero-body
       .container.has-text-centered
         h1.title PlatziMusic
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Search from '@/components/Search'
+import About from '@/components/About'
 
 const routes = [
-  { path: '/', component: Search, name: 'search' }
+  { path: '/', component: Search, name: 'search' },
+  { path: '/about', component: About, name: 'about' }
 ]
 
 export default routes
